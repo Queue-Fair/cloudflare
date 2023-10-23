72cd356792100b7679e710683ed103e57e632e67
Queue-Fair <87873163+Queue-Fair@users.noreply.github.com>
2023-10-24 00:30:43 +0100

    Update worker.js
    Added support for RegExp rules.

--- worker.js
+++ worker.js
@@ -523,7 +523,13 @@ class QueueFairAdapter {
       } else {
         ret=true;
       }
-    }
+    } else if (rule.match == "RegExp") {
+		  if(typeof comp == 'undefined' || comp === null) {
+			  comp = "";
+			}
+			var r = new RegExp(test);
+			ret = r.test(comp);
+		}
 
     if (rule.negate) {
       ret=!ret;
